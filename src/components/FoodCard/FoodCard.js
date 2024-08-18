66afbafaab58a1f74136fbbff218b0e922cbcded
ankitakangane <kanganeankita1@gmail.com>
2024-08-18 12:33:52 +0530

    price added to card

--- src/components/FoodCard/FoodCard.js
+++ src/components/FoodCard/FoodCard.js
@@ -3,13 +3,20 @@ import "./FoodCard.css";
 import veg from "./veg (2).png";
 import non_veg from "./non veg.png"
 
-function FoodCard({imgUrl,title,discription,isVeg}) {
+function FoodCard({imgUrl,title,discription,isVeg,price}) {
   return (
     <div className="food_card">
         <img src={imgUrl} className='food_img'/>
       <h1 className="food_title">{title}</h1>
       <p>{discription}</p>
       <img src={isVeg ? veg : non_veg} className="food_icons"/>
+      {
+      price ? <p className='food_price'>Rs {price} </p>
+      : "Not Available"
+      }
+      <button type="button" className='food_butn'>
+        Add to Card
+      </button>
     </div>
   )
 }
