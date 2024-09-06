b3f7f8d7f1ebae7174804b5b40b2b227847456b7
ankitakangane <kanganeankita1@gmail.com>
2024-09-06 06:08:07 +0530

    use configurable ui

--- src/components/FoodCard/FoodCard.js
+++ src/components/FoodCard/FoodCard.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import "./FoodCard.css";
 import veg from "./veg (2).png";
-import non_veg from "./non veg.png"
+import non_veg from "./non veg.png";
+import {THEME} from "./../../config/data"
 
 function FoodCard({imgUrl,title,discription,isVeg,price}) {
   return (
     <div className="food_card">
         <img src={imgUrl} className='food_img'/>
-      <h1 className="food_title">{title}</h1>
+      <h1 className="food_title" style={{color:THEME.highlight}}>{title}</h1>
       <p>{discription}</p>
       <img src={isVeg ? veg : non_veg} className="food_icons"/>
       {
       price ? <p className='food_price'>Rs {price} </p>
       : "Not Available"
       }
-      <button type="button" className='food_butn'>
+      <button type="button" className='food_butn' style={{backgroundColor:THEME.highlight}}>
         Add to Card
       </button>
     </div>
---
+++ src/config/data.js
@@ -0,0 +1,64 @@
+const TITLE = "Ankita Da Dhaba 🍕";
+
+const TAGLINE = "Best Food !!!!";
+
+const THEME = {
+    main:"#fffffe",
+    highlight:"#ff8ba7",
+    tertiary:"#e6f9ff",
+    dark:"#e6f2ff",
+    secondary:"#ffc6c7"
+}
+
+const PRODUCTS = [
+    {
+      imgUrl:"https://media.istockphoto.com/id/1345624336/photo/chicken-biriyani.jpg?s=1024x1024&w=is&k=20&c=bvTAMlq5A8Z5EhVjBn6D8eYOQS-rsuKmT9ToLkCc2Y4=",
+      title:"Chiken Biryani",
+      discription:" 'Chicken Biryani' is a highly aromatic, mouth-watering staple dish that needs no introduction",
+      isVeg:false,
+      price:200
+    },
+    {
+      imgUrl:"https://img.freepik.com/premium-photo/photography-tasty-indian-masala-dosa_1288657-45837.jpg?w=740",
+      title:"Dosa",
+      discription:" 'Dosa' is a highly aromatic, mouth-watering staple dish that needs no introduction",
+      isVeg:true,
+      price:100
+    },
+    {
+      imgUrl:"https://img.freepik.com/free-photo/side-view-club-sandwich-with-salted-cucumbers-lemon-olives-round-white-plate_176474-3049.jpg?t=st=1723903755~exp=1723907355~hmac=9443fc09e98eb04850c5778ea61474974dd220826b1eb0f7adfdd0d38fa8bedc&w=740",
+      title:"Sandwich",
+      discription:" 'Sandwich' is a highly aromatic, mouth-watering staple dish that needs no introduction",
+      isVeg:true,
+      price:80
+    },
+    {
+      imgUrl:"https://img.freepik.com/premium-photo/ulta-vada-pav-is-made-with-spicy-potato-stuffed-bun-called-pav-inside-vada-inside-out-wada-pao_466689-86255.jpg?w=900",
+      title:"Wada Pav",
+      discription:" 'Wada Pav' is a highly aromatic, mouth-watering staple dish that needs no introduction",
+      isVeg:true,
+      price:50
+    },
+    {
+      imgUrl:"https://as1.ftcdn.net/v2/jpg/04/94/92/18/1000_F_494921881_UCVG7kgihpMUbbDC5VxuufeI7B5TQBr0.jpg",
+      title:"Pani-Puri",
+      discription:" 'Pani-Puri' is a highly aromatic, mouth-watering staple dish that needs no introduction",
+      isVeg:true,
+      price:50
+    },
+    {
+      imgUrl:"https://img.freepik.com/premium-photo/3d-rendered-photo-chicken-qorma-with-simple-background_1149190-12572.jpg?w=740",
+      title:"Chicken",
+      discription:" 'Chicken' is a highly aromatic, mouth-watering staple dish that needs no introduction",
+      isVeg:false,
+      price:50
+    }
+  ]
+  
+
+  export {
+    PRODUCTS,
+    TITLE,
+    TAGLINE,
+    THEME
+  }
